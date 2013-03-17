983ef97691a47f4f5201eb6039a0283b0ada52e2
Daniel Martin <etd-github@nomejortu.com>
2013-03-17 18:43:48 +0100

    Include Dradis::Core::Plugins::Base in the main module

--- lib/dradis/plugins/osvdb.rb
+++ lib/dradis/plugins/osvdb.rb
@@ -6,11 +6,17 @@ require 'dradis/plugins/osvdb/version'
 module Dradis
   module Plugins
     module OSVDB
-      BAD_API_KEY = '<your_API_key>'
+      # Connect to the Framework
+      include Dradis::Core::Plugins::Base
+
+      # These are provided by the above Plugins::Base
+      self.plugin_name= 'OSVDB import'
+      provides :import
 
       # Please register an account in the OSVDB site to get your API key. Steps:
       #   1. Create the account: http://osvdb.org/account/signup
       #   2. Find your key in http://osvdb.org/api
+      BAD_API_KEY = '<your_API_key>'
       class Configuration < Dradis::Core::Configurator
         configure     :namespace => 'osvdb'
         setting       :api_key, :default => BAD_API_KEY
--- lib/dradis/plugins/osvdb/engine.rb
+++ lib/dradis/plugins/osvdb/engine.rb
@@ -2,7 +2,6 @@ module Dradis
   module Plugins
     module OSVDB
       class Engine < ::Rails::Engine
-
         # Standard Rails Engine stuff
         isolate_namespace Dradis::Plugins::OSVDB
         engine_name 'dradis_osvdb'
@@ -11,14 +10,6 @@ module Dradis
         config.generators do |g|
           g.test_framework :rspec
         end
-
-        # Connect to the Framework
-        include Dradis::Core::Plugins::Base
-
-        plugin_name 'OSVDB import'
-        provides :import
-
-        Dradis::Core::Plugins::register(Dradis::Plugins::OSVDB)
 
         # No need to initialize anything in this case
         # initializer "dradis.osvdb.<>" do |app|
